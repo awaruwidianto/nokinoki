f5f6e8b5f62008d92aad66cc6bbec0d52f32cf9d
awaruwidianto <awaruwidianto@gmail.com>
2026-07-09 21:13:37 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+// Service Worker for Noki Noki
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
